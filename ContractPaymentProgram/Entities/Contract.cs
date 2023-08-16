7378d7801679db9083da3f5b6098749318452da6
Nathan-Barbosa <nathan.s.barbosa01@gmail.com>
2023-08-16 15:54:42 -0300

    Add project files.

---
+++ ContractPaymentProgram/Entities/Contract.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContractPaymentProgram.Entities
+{
+    internal class Contract
+    {
+        public int Number { get; set; }
+        public DateTime Date { get; set; }
+        public double TotalValue { get; set; }
+
+        public List<Installment> Installments { get; set; }
+        public Contract() { }
+
+        public Contract(int number, DateTime date, double totalPayment)
+        {
+            Number = number;
+            Date = date;
+            TotalValue = totalPayment;
+            Installments = new List<Installment>();
+        }
+
+        public void AddInstallment(Installment installment)
+        {
+            Installments.Add(installment);
+        }
+    }
+}
---
+++ ContractPaymentProgram/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using ContractPaymentProgram.Entities;
+using ContractPaymentProgram.Services;
+
+namespace ContractPaymentProgram
+{
+    class Program
+    {
+        public static void Main(string[] args) {
+
+            Console.WriteLine("Enter contract date");
+            Console.Write("Number: ");
+            int contractNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Date (dd/MM/yyyy): ");
+            DateTime contractDate = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Contract value: ");
+            double contractValue = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter number of installments: ");
+            int months = int.Parse(Console.ReadLine());
+
+            Contract contract = new Contract (contractNumber, contractDate, contractValue);
+
+            ContractService contractService = new ContractService(new PayPalService());
+
+            contractService.ProcessContract(contract, months);
+
+            Console.WriteLine("Installments: ");
+            foreach(Installment installment in contract.Installments)
+            {
+                Console.WriteLine(installment);
+            }
+        }
+    }
+}
